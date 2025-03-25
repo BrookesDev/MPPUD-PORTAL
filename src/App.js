dba90f2e00c9167e2f812e3681b1fc4ce4859dc3
Samstick17 <holuwahdahmelahrey@gmail.com>
2025-03-25 09:32:04 +0100

    New

--- src/App.js
+++ src/App.js
@@ -31,6 +31,8 @@ import Allinvoices from "./Pages/All Invoices/Allinvoices.js";
 import AllPayment from "./Pages/All Payments/AllPayment.js";
 import NewApplications from "./Pages/New Applicationss/NewApplications.js";
 import LandRatificationApp from "./Pages/New Applicationss/LandRatificationApplication.js";
+import OnboardingCompleteReg from "./Pages/NewSignup/OnboardingCompleteReg.js";
+import FinishOnboarding from "./Pages/Finish Onboarding/FinishOnboarding.js";
 
 
 function App() {
@@ -59,6 +61,8 @@ function App() {
             <Route path="/create" element={<Create />} />
             <Route path="/" element={<Welcome />} />
             <Route path="/account" element={<Account />} />
+            <Route path="/complete_your_registration" element={<OnboardingCompleteReg />} />
+            <Route path="/finish_onboarding_process" element={<FinishOnboarding />} />
             <Route path="/application" element={<Application />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgot_password" element={<ForgotPassword />} />
