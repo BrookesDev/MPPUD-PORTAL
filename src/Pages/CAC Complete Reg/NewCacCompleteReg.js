0e1e8b03161f20709cbb52e7803d21e8b7fa6fde
samuel <holuwahdahmelahrey@gmail.com>
2026-02-27 16:27:45 +0100

    new update

--- src/Pages/CAC Complete Reg/NewCacCompleteReg.js
+++ src/Pages/CAC Complete Reg/NewCacCompleteReg.js
@@ -19,6 +19,7 @@ import axios from "axios";
 // import localStorage from "@react-native-async-storage/async-storage";
 import Valid from '../../Asset/valid.png';
 import Invalid from '../../Asset/invalid.png';
+import ogunlogo from "../../Asset/ogunlogonew.svg"
 
 
 
@@ -245,62 +246,44 @@ function NewCacCompleteReg() {
   return (
     <div >
       <div className={classes.maincontainer}>
-        <div className={classes.lftcontainer}>
-          <div className={classes.logohead}>
-            <img src={logo} alt="Olarms Logo" className={classes.logoimage} />
-          </div>
-
-          <Carousel
-            activeIndex={activeIndex}
-            onSelect={handleSelect}
-            controls={false}
-            indicators={false}
-            className={classes.customCarousel}
-          >
-            <Carousel.Item className={classes.textdown}>
-              <h6 className={classes.textdownH}>
-                {" "}
-                Seamless Land <br />
-                Administration Starts Here
-              </h6>
-              <p className={classes.textdownP}>
-                Welcome to the Ogun State Land Administration <br />
-                and Revenue Management System (OLARMS).
-              </p>{" "}
-            </Carousel.Item>
-            <Carousel.Item className={classes.textdown}>
-              <h6 className={classes.textdownH}>
-                {" "}
-                Your Gateway to Owning Land <br />
-                Starts Here!
-              </h6>
-              <p className={classes.textdownP}>
-                Discover the simplicity of land ownership with Ogun State's <br />
-                premier Land Administration and Revenue Management System.
-              </p>{" "}
-            </Carousel.Item>
-            <Carousel.Item className={classes.textdown}>
-              <h6 className={classes.textdownH}>
-                {" "}
-                Invest in Your Future <br />
-                with Land in Ogun State!
-              </h6>
-              <p className={classes.textdownP}>
-                Unlock seamless access to affordable and secure land <br />
-                ownership through Ogun State’s trusted OLARMS.
-              </p>{" "}
-            </Carousel.Item>
-          </Carousel>
-          <div className={classes.slidehead4}>
-            {[0, 1, 2].map((index) => (
-              <span
-                key={index}
-                onClick={() => handleSelect(index)}
-                className={`${classes.indicator} ${activeIndex === index ? classes.activeIndicator : ""
-                  }`}
-              ></span>
-            ))}
-          </div>
+      <div className={classes.lftcontainer}>
+          <Row>
+            <Col md={12} style={{ display: 'flex', justifyContent: 'center' }}>
+              <img src={ogunlogo} className={classes.logoimage} alt="logo" />
+            </Col>
+            <Col md={12}>
+              <h1
+                style={{
+                  color: 'white',
+                  fontSize: '30px',
+                  fontWeight: '600',
+                  fontFamily: 'Inter, sans-serif',
+                  margin: 0,
+                  textAlign: 'center',
+                }}>
+                MPPUD PORTAL
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+            </Col>
+            <Col md={12}>
+              <p
+                style={{
+                  color: 'white',
+                  fontSize: '14px',
+                  fontWeight: '400',
+                  fontFamily: 'Inter, sans-serif',
+                  textAlign: 'center',
+                  margin: 0,
+                  maxWidth: "450px",
+                }}>
+                Welcome to the Ogun State Ministry of Physical Planning and Urban Development (MPPUD)  Application Portal  
+                {/* Effortless Budget Management: Request, Track, and<br />Approve with Ease */}
+              </p>
+            </Col>
+          </Row>
         </div>
         <div className={classes.rgtcontainer}>
           <div className={classes.maintext}>
